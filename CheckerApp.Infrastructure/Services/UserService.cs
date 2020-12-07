e2b70b9f657905ddceaac589a26a111b6abb0ccc
Odis <tracemode@mail.ru>
2020-12-07 08:51:39 +0300

    изменение в UserService

--- CheckerApp.Infrastructure/Services/UserService.cs
+++ CheckerApp.Infrastructure/Services/UserService.cs
@@ -1,7 +1,6 @@
 ﻿using CheckerApp.Application.Common.Interfaces;
 using CheckerApp.Domain.Entities.Identity;
 using Microsoft.AspNetCore.Identity;
-using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using System.Threading.Tasks;
 
@@ -9,11 +8,6 @@ namespace CheckerApp.Infrastructure.Services
 {
     public class UserService : IUserService
     {
-        private const string DisplayNameAttribute = "DisplayName";
-        private const string GivenNameAttribute = "GivenName";
-        private const string SnAttribute = "Sn";
-        private const string SAMAccountNameAttribute = "SAMAccountName";
-
         private readonly UserManager<ApplicationUser> _userManager;
 
         public UserService(UserManager<ApplicationUser> userManager)
@@ -23,64 +17,38 @@ namespace CheckerApp.Infrastructure.Services
 
         public async Task<ApplicationUser> GetUserAsync(string username, string password)
         {
-            using (var context = new PrincipalContext(ContextType.Domain, "INCOMSYSTEM.ru"))
+            using (var context = new PrincipalContext(ContextType.Domain, "INCOMSYSTEM.ru", username, password))
             {
-                // Проверка логина и пароля
+                ApplicationUser user = null;
                 if (context.ValidateCredentials(username, password))
                 {
-                    var user = await _userManager.FindByNameAsync(username);
+                    user = await _userManager.FindByNameAsync(username);
 
                     if (user == null)
                     {
-                        // Поиск пользователя в ActiveDirectory
-                        using (var entry = new DirectoryEntry("LDAP://incomsystem.ru", "INCOMSYSTEM\\" + username, password))
+                        var userPrincipal = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username);
+
+                        var nameArray = userPrincipal.DisplayName.Split(' ');
+
+                        user = new ApplicationUser
                         {
-                            using (var searcher = new DirectorySearcher(entry))
-                            {
-                                // Критерий поиска
-                                searcher.Filter = $"SAMAccountName={username}";
+                            UserName = username,
+                            FullName = $"{nameArray[1]} {nameArray[0]}"
+                        };
 
-                                // Получаемые свойства
-                                searcher.PropertiesToLoad.Add(SAMAccountNameAttribute);
-                                searcher.PropertiesToLoad.Add(DisplayNameAttribute);
-                                searcher.PropertiesToLoad.Add(GivenNameAttribute);
-                                searcher.PropertiesToLoad.Add(SnAttribute);
+                        await _userManager.CreateAsync(user);
 
-                                var result = searcher.FindOne();
-
-                                if (result != null)
-                                {
-                                    var accountName = result.Properties[SAMAccountNameAttribute][0].ToString();
-                                    var displayName = result.Properties[DisplayNameAttribute][0].ToString();
-
-                                    var nameArray = displayName.Split(' ');
-
-                                    var name = nameArray[1];
-                                    var lastName = nameArray[0];
-
-                                    user = new ApplicationUser
-                                    {
-                                        UserName = accountName,
-                                        FullName = $"{lastName} {name}"
-                                    };
-
-                                    await _userManager.CreateAsync(user);
-
-                                    if (user.UserName.Equals("budanovav", System.StringComparison.OrdinalIgnoreCase))
-                                    {
-                                        await _userManager.AddToRoleAsync(user, "Admin");
-                                    }
-                                    else
-                                    {
-                                        await _userManager.AddToRoleAsync(user, "User");
-                                    }
-                                }
-                            }
+                        if (user.UserName.Equals("budanovav", System.StringComparison.OrdinalIgnoreCase))
+                        {
+                            await _userManager.AddToRoleAsync(user, "Admin");
+                        }
+                        else
+                        {
+                            await _userManager.AddToRoleAsync(user, "User");
                         }
                     }
-                    return user;
                 }
-                return null;
+                return user;
             }
         }
     }
