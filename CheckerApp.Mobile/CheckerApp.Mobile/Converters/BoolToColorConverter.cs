44cf9a6a9940888311901b7c66e3779131ab9fb9
Odis <tracemode@mail.ru>
2021-02-25 11:58:13 +0300

    работа над экраном договоров

--- CheckerApp.Mobile/CheckerApp.Mobile/Converters/BoolToColorConverter.cs
+++ CheckerApp.Mobile/CheckerApp.Mobile/Converters/BoolToColorConverter.cs
@@ -8,7 +8,7 @@ namespace CheckerApp.Mobile.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((bool)value) ? Device.GetNamedColor("Green") : Device.GetNamedColor("Red");
+            return ((bool)value) ? Color.LightGreen : Color.Red;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
